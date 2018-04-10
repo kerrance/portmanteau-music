6897e6afac0147b271d157bd658646c948f0b927
Kerri Lush <hello@kerrilu.sh>
2018-04-10 10:06:12 +0100

    Fix indentation and code style for 'about' segment

--- about.php
+++ about.php
@@ -1,6 +1,14 @@
-				<div id="column_about">
-                <span class="entry_header">Our Music</span>
-Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands.
-                </div>
-                <div class="about_text">Portmanteau is a UK based music licensing agency exclusively representing a range of European record labels, publishers and artists in order to place their music in audio-visual productions around the world. We represent both master and publishing copyrights in most cases and so can clear for use quickly and in 'one-stop'.
-                </div>
+<div id="column_about">
+	<span class="entry_header">
+		Our Music
+	</span>
+	<p>
+		Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands.
+	</p>
+</div>
+
+<div class="about_text">
+	<p>
+		Portmanteau is a UK based music licensing agency exclusively representing a range of European record labels, publishers and artists in order to place their music in audio-visual productions around the world. We represent both master and publishing copyrights in most cases and so can clear for use quickly and in 'one-stop'.
+	</p>
+</div>
