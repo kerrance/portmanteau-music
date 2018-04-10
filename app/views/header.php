8d8c654f5e6cc5150c0b579940a9f8e62622944b
Kerri Lush <hello@kerrilu.sh>
2018-04-10 15:15:50 +0100

    Include various sizes of favicons instead of just one

--- app/views/header.php
+++ app/views/header.php
@@ -7,7 +7,27 @@
 	<meta name="title" content="Portmanteau | portmanteaumusic.com" />
 	<meta name="description" content="Portmanteau is a UK based music licensing agency exclusively representing a range of European record labels, publishers and artists in order to place their music in audio-visual productions around the world. We represent both master and publishing copyrights in most cases and so can clear for use quickly and in 'one-stop'. Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands." />
 	<link rel="image_src" href="/images/Portmanteau_LogoSquare.jpg" />
-	<link rel="shortcut icon" href="/images/favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="/images/icons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="/images/icons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="/images/icons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="/images/icons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="/images/icons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/icons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="/images/icons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/icons/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="/images/icons/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="/images/icons/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="/images/icons/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="/images/icons/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="/images/icons/favicon-128.png" sizes="128x128" />
+	<meta name="application-name" content="Portmanteau Music"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="/images/icons/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="/images/icons/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="/images/icons/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="/images/icons/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="/images/icons/mstile-310x310.png" />
+
 	<script>
 		window.fbAsyncInit = function() {
 			FB.init({appId: 'your app id', status: true, cookie: true,
