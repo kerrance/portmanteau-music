1c88dc288d9aace2b45ec646974961404c8fc3bf
Kerri Lush <hello@kerrilu.sh>
2018-04-10 10:08:50 +0100

    Fix line-break code style in footer

--- footer.php
+++ footer.php
@@ -4,15 +4,16 @@
 			<div class="limit">
 				<span style="float: left">
 					<span class="exclaim_half">
-						About<br>
+						About <br />
 					</span>
-					Portmanteau is a UK based music licensing agency<br>for audio-visual productions from around the world.<br><br>
+					Portmanteau is a UK based music licensing agency <br />
+					for audio-visual productions from around the world. <br /><br />
 					Website by <a href="https://kerrilu.sh" target="_blank">Kerri Lush</a>. <br /><br />
 					&copy; <?php echo date('Y'); ?> <a href="http://portmanteauagency.com">Portmanteau Agency</a>.
 				</span>
 				<span style="float: right">
 					<span class="exclaim_half">
-						Contact<br>
+						Contact <br />
 					</span>
 					<dl>
 						<dt>Email</dt>
@@ -30,7 +31,12 @@
 						<dt>Phone</dt>
 						<dd>+44 (0) 1603 632466</dd>
 						<dt>Address</dt>
-						<dd>1 St. Augustines Gate<br>Norwich<br>NR3 3BE<br>United Kingdom</dd>
+						<dd>
+							1 St. Augustines Gate <br />
+							Norwich <br />
+							NR3 3BE <br />
+							United Kingdom
+						</dd>
 					</dl> 
 				</span>
 			</div>
