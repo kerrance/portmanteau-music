186153b9f381a0a99f8950dff73ba846937df45b
Kerri Lush <hello@kerrilu.sh>
2018-04-10 10:09:51 +0100

    Fix code indentation on index constructor

--- index.php
+++ index.php
@@ -1,46 +1,48 @@
 <?php include 'header.php'; ?>
 
-			<div id="portmanteaubacking">
-				<div class="limit" style="margin: -20px auto; padding: 20px 0;">
-					        
-		            <span class="exclaim_half" style="float: left; margin:0 0 20px 0;">         	
-						Portmanteau represents a range of record labels, publishers and artists in order to place their music in audio-visual productions from around the world - online, on television and in film. We cover both master and publishing copyrights in the majority of cases and so can clear for use quickly and in 'one-stop'.
-		            </span>
-		                Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands.    
-		        </div>	        
-			</div>
-            
-            <div class="featured_wrapper">
-	            <div class="limit">
-	            	<span class="exclaim_half">
-	            	Some of our recent placements...
-	            	</span>
-	            </div>
-            </div>
-            
-            <div class="limit" style="margin: 20px auto;">
-            	<!-- 2015 -->
-            	<? include('portfolio/thewalkingdead.php'); ?> 
-            	<? include('portfolio/extant.php'); ?> 
-				<!-- 2014 and earlier -->
-            	<? include('portfolio/adidas2.php'); ?> 
-            	<? include('portfolio/freeview.php'); ?> 
-            	<? include('portfolio/adidas.php'); ?> 
-            	<? include('portfolio/mcdonalds.php'); ?> 
-            	<? include('portfolio/lenovo.php'); ?> 
-            	<? include('portfolio/thecanyons.php'); ?> 
-            	<? include('portfolio/thefollowing.php'); ?> 
-            	<? include('portfolio/theironlady.php'); ?> 
-            	<? include('portfolio/royalpains.php'); ?> 
-            	<? include('portfolio/battleoftheexes.php'); ?> 
-            	<? include('portfolio/kinectstarwars.php'); ?> 
-            	<? include('portfolio/thesacred.php'); ?> 
-            	<? include('portfolio/thechallengerivals.php'); ?> 
-            	<? include('portfolio/dirt3.php'); ?> 
-            	<? include('portfolio/fastfive.php'); ?> 
-            	<? include('portfolio/testdriveunlimited2.php'); ?>
-            	<? include('portfolio/trueblood.php'); ?> 
-            	<? include('portfolio/onetreehill.php'); ?>
-            </div>
+<div id="portmanteaubacking">
+    <div class="limit" style="margin: -20px auto; padding: 20px 0;">
+        <span class="exclaim_half" style="float: left; margin:0 0 20px 0;">         	
+            Portmanteau represents a range of record labels, publishers and artists in order to place their music in audio-visual productions from around the world - online, on television and in film. We cover both master and publishing copyrights in the majority of cases and so can clear for use quickly and in 'one-stop'.
+        </span>
+
+        <p>
+            Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands.  
+        </p>  
+    </div>	        
+</div>
+
+<div class="featured_wrapper">
+    <div class="limit">
+        <span class="exclaim_half">
+            Some of our recent placements...
+        </span>
+    </div>
+</div>
+
+<div class="limit" style="margin: 20px auto;">
+    <!-- 2015 -->
+    <? include('portfolio/thewalkingdead.php'); ?> 
+    <? include('portfolio/extant.php'); ?> 
+    <!-- 2014 and earlier -->
+    <? include('portfolio/adidas2.php'); ?> 
+    <? include('portfolio/freeview.php'); ?> 
+    <? include('portfolio/adidas.php'); ?> 
+    <? include('portfolio/mcdonalds.php'); ?> 
+    <? include('portfolio/lenovo.php'); ?> 
+    <? include('portfolio/thecanyons.php'); ?> 
+    <? include('portfolio/thefollowing.php'); ?> 
+    <? include('portfolio/theironlady.php'); ?> 
+    <? include('portfolio/royalpains.php'); ?> 
+    <? include('portfolio/battleoftheexes.php'); ?> 
+    <? include('portfolio/kinectstarwars.php'); ?> 
+    <? include('portfolio/thesacred.php'); ?> 
+    <? include('portfolio/thechallengerivals.php'); ?> 
+    <? include('portfolio/dirt3.php'); ?> 
+    <? include('portfolio/fastfive.php'); ?> 
+    <? include('portfolio/testdriveunlimited2.php'); ?>
+    <? include('portfolio/trueblood.php'); ?> 
+    <? include('portfolio/onetreehill.php'); ?>
+</div>
 
 <?php include 'footer.php'; ?>
