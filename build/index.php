51f8e2531ebe650d9603cb74f5593cf9d0f21f3b
Kerri Lush <hello@kerrilu.sh>
2018-04-11 12:26:49 +0100

    Compile Assets

--- build/index.php
+++ build/index.php
@@ -2,6 +2,21 @@
 require_once 'views/header.php';
 ?> <?php
 require_once 'views/hero.php';
-?> <div id="content"><div class="featured_wrapper"><div class="limit"><span class="exclaim_half">Some of our recent placements...</span></div></div><div class="limit" style="margin: 20px auto"> <?php include('portfolio/thewalkingdead.php'); ?> <?php include('portfolio/extant.php'); ?> <?php include('portfolio/adidas2.php'); ?> <?php include('portfolio/freeview.php'); ?> <?php include('portfolio/adidas.php'); ?> <?php include('portfolio/mcdonalds.php'); ?> <?php include('portfolio/lenovo.php'); ?> <?php include('portfolio/thecanyons.php'); ?> <?php include('portfolio/thefollowing.php'); ?> <?php include('portfolio/theironlady.php'); ?> <?php include('portfolio/royalpains.php'); ?> <?php include('portfolio/battleoftheexes.php'); ?> <?php include('portfolio/kinectstarwars.php'); ?> <?php include('portfolio/thesacred.php'); ?> <?php include('portfolio/thechallengerivals.php'); ?> <?php include('portfolio/dirt3.php'); ?> <?php include('portfolio/fastfive.php'); ?> <?php include('portfolio/testdriveunlimited2.php'); ?> <?php include('portfolio/trueblood.php'); ?> <?php include('portfolio/onetreehill.php'); ?> </div></div> <?php
+?> <div class="content"><div class="feature"><div class="container"><div class="col-md-12"><p class="exclaim half">Some of our recent placements...</p></div></div></div><div class="placements"><div class="container"> <?php
+            $slug = "mcdonalds";
+            $embed = "//www.youtube.com/embed/qIvY73HfJWM";
+            $image = "/images/portfolio/mcdonalds.jpg";
+            $project = "Parallel Lives";
+            $client = "McDonald's";
+            $placements = [
+                "'Like A Boss' by Eptic",
+                "'Sick Style' (feat. Maksim) <br />
+                (Dub Mix) by Habstrakt"
+            ];
+
+            include 'partials/placement.php';
+
+            unset($slug, $embed, $image, $project, $client, $placements);
+            ?> <!-- <?php include('portfolio/thewalkingdead.php'); ?> <?php include('portfolio/extant.php'); ?> <?php include('portfolio/adidas2.php'); ?> <?php include('portfolio/freeview.php'); ?> <?php include('portfolio/adidas.php'); ?> <?php include('portfolio/mcdonalds.php'); ?> <?php include('portfolio/lenovo.php'); ?> <?php include('portfolio/thecanyons.php'); ?> <?php include('portfolio/thefollowing.php'); ?> <?php include('portfolio/theironlady.php'); ?> <?php include('portfolio/royalpains.php'); ?> <?php include('portfolio/battleoftheexes.php'); ?> <?php include('portfolio/kinectstarwars.php'); ?> <?php include('portfolio/thesacred.php'); ?> <?php include('portfolio/thechallengerivals.php'); ?> <?php include('portfolio/dirt3.php'); ?> <?php include('portfolio/fastfive.php'); ?> <?php include('portfolio/testdriveunlimited2.php'); ?> <?php include('portfolio/trueblood.php'); ?> <?php include('portfolio/onetreehill.php'); ?> --></div></div></div> <?php
 require_once 'views/footer.php';
 ?>
---
+++ build/partials/placement.php
@@ -0,0 +1,14 @@
+<section class="placement" <?php
+if(isset($slug)) {
+    echo ' id="' . strtolower($slug) . '"';
+}
+?> ><div class="col-xs-12 col-sm-6"> <?php
+    if(isset($embed)) {
+        echo '<iframe width="340" height="191" src="' . $embed . '" frameborder="0" allowfullscreen></iframe>';
+    } else {
+        echo '<img src="' . $image . '">"';
+    };
+    ?> <dl><dt>Project</dt><dd><?php echo $project ?></dd><dt>Client</dt><dd><?php echo $client ?></dd><dt>Placement</dt> <?php foreach($placements as $placement) {
+            echo '<dd>' . $placement . '</dd>';
+        } 
+        ?> </dl></div></section>
