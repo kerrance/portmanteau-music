a5af300508085ab235127cf89a6fb24cadfc73d6
Kerri Lush <hello@kerrilu.sh>
2018-04-11 12:25:40 +0100

    Re-factor footer area for responsive display

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,34 +1,49 @@
 		<footer>
-			<div class="limit">
-				<span style="float: left">
-					<span class="exclaim_half">
-						About <br />
-					</span>
-					Portmanteau is a UK based music licensing agency <br />
-					for audio-visual productions from around the world. <br /><br />
-					Website by <a href="https://kerrilu.sh" target="_blank">Kerri Lush</a>. <br /><br />
-					&copy; <?php echo date('Y'); ?> <a href="https://portmanteauagency.com">Portmanteau Agency</a>.
-				</span>
-				<span style="float: right">
-					<span class="exclaim_half">
-						Contact <br />
-					</span>
+			<div class="container">
+				<div class="col-xs-12 col-sm-6">
+					<p class="exclaim half">
+						About
+					</p>
+					<p>
+						Portmanteau is a UK based music licensing agency for audio-visual productions from around the world. 
+					</p>
+					<p>
+						Website by <a href="https://kerrilu.sh" target="_blank">Kerri Lush</a>.
+					</p>
+					<p>
+						&copy; <?php echo date('Y'); ?> <a href="https://portmanteauagency.com">Portmanteau Agency</a>.
+					</p>
+				</div>
+				<div class="col-xs-12 col-sm-6">
+					<p class="exclaim half">
+						Contact
+					</p>
 					<dl>
-						<dt>Email</dt>
+						<dt>
+							Email
+						</dt>
 						<dd>
 							<a href="mailto:info@portmanteaumusic.com">
 								info@portmanteaumusic.com
 							</a>
 						</dd>
-						<dt>Twitter</dt>
+						<dt>
+							Twitter
+						</dt>
 						<dd>
 							<a href="https://twitter.com/PortmanteauUK" target="_blank">
 								@PortmanteauUK
 							</a>
 						</dd>
-						<dt>Phone</dt>
-						<dd>+44 (0) 1603 632466</dd>
-						<dt>Address</dt>
+						<dt>
+							Phone
+						</dt>
+						<dd>
+							+44 (0) 1603 632466
+						</dd>
+						<dt>
+							Address
+						</dt>
 						<dd>
 							1 St. Augustines Gate <br />
 							Norwich <br />
@@ -36,7 +51,7 @@
 							United Kingdom
 						</dd>
 					</dl> 
-				</span>
+				</div>
 			</div>
 		</footer>
 	</div>
