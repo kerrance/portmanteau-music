fbb765d0e8f175bc9728859117b1656ab5cefa0d
Kerri Lush <hello@kerrilu.sh>
2018-04-11 11:13:17 +0100

    Properly vertically center hero banner text

--- app/views/hero.php
+++ app/views/hero.php
@@ -8,6 +8,6 @@
 			<p>
 				Our extensive yet select catalogue of labels and publishers is carefully picked in order to offer quality and diversity covering many genres of music with a particular focus on cutting edge, emerging artists and 'buzz' bands.  
 			</p>  
-		</div>	 
+		</div>
 	</div>       
 </div>
